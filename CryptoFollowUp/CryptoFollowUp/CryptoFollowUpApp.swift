16bf01bb0f863fb63698d566d0261d4667567e15
ihan <ihancarlosihan@gmail.com>
2026-05-02 16:29:16 -0300

    First configutarions

---
+++ CryptoFollowUp/CryptoFollowUp/CryptoFollowUpApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoFollowUpApp.swift
+//  CryptoFollowUp
+//
+//  Created by ihan carlos on 02/05/26.
+//
+
+import SwiftUI
+
+@main
+struct CryptoFollowUpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CryptoFollowUp/CryptoFollowUpTests/CryptoFollowUpTests.swift
@@ -0,0 +1,19 @@
+//
+//  CryptoFollowUpTests.swift
+//  CryptoFollowUpTests
+//
+//  Created by ihan carlos on 02/05/26.
+//
+
+import Testing
+@testable import CryptoFollowUp
+
+struct CryptoFollowUpTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
